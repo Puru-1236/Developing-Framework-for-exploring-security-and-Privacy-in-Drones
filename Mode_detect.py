46a3312585340200d225b6470aacd11f5e39d2fd
Purushottam Bairwa <70768173+Puru-1236@users.noreply.github.com>
2021-05-26 11:16:37 +0530

    Create Mode_detect.py

---
+++ Mode_detect.py
@@ -0,0 +1,22 @@
+from dronekit import connect, VehicleMode
+import sys
+import time
+
+
+# Connect to the Vehicle (in this case a UDP endpoint)
+vehicle = connect('127.0.0.1:14550', wait_ready=True)
+
+#vehicle.mode=VehicleMode("GUIDED")
+if vehicle.mode.name!= 'STABILIZE':
+	print('Attacker changed the mode of the drone')
+else:
+	print("Mode is correct showing according to legitimate user action: %s" % vehicle.mode.name)
+#print(" Home Location: %s" % vehicle.home_location)
+print("Global Location: %s" % vehicle.location.global_frame)
+print("Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+print("Local Location: %s" % vehicle.location.local_frame)
+print("Local Location: %s" % vehicle.location.local_frame.north)
+#if vehicle.location.local_frame.north >1:
+#	for i in range(10):	
+#		print("Attack happened on drone, need to return to launch position, please hit the alarm")
+#	vehicle.mode=VehicleMode("RTL")
